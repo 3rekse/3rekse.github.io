cb25c8fbd7520663f91a7ad83ad5c7a49051bada
3rekse <bonfiglio.fabrizio@itiomar.net>
2024-05-03 13:08:53 +0200

    Updates

---
+++ assets/slidev/161-BI1KdStI.js
@@ -0,0 +1,15 @@
+import{o as e,c as t,k as i,q as p,s as o,A as l,e as s,a6 as n}from"../modules/vue-CzHRG3tJ.js";import{I as r}from"./image-right-C8QtEv9p.js";import{_ as c,cK as a}from"../index-CNiOs4Um.js";import{p as u,u as f,f as d}from"./context-CzMzD048.js";import"./layoutHelper-C4hfVeg5.js";import"../modules/shiki-C5i64R19.js";const _=s("h2",null,"Two input loop",-1),h=s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-"},[s("span",{class:"line"},[s("span",null,"let inputs = [false, true]")]),n(`
+`),s("span",{class:"line"},[s("span",null,"let Q = false")]),n(`
+`),s("span",{class:"line"},[s("span",null,"for (let A of inputs) {")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    for (let B of inputs) {")]),n(`
+`),s("span",{class:"line"},[s("span",null,"        Q = !A")]),n(`
+`),s("span",{class:"line"},[s("span",null,"        if (Q) {")]),n(`
+`),s("span",{class:"line"},[s("span",null,"            light.setAll(0x0000ff)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"        } else {")]),n(`
+`),s("span",{class:"line"},[s("span",null,"            light.setAll(0xff8000)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"        }")]),n(`
+`),s("span",{class:"line"},[s("span",null,"        pause(500)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"        light.clear()")]),n(`
+`),s("span",{class:"line"},[s("span",null,"        pause(500)")]),n(`
+`),s("span",{class:"line"},[s("span",null,"    }")]),n(`
+`),s("span",{class:"line"},[s("span",null,"}")])])],-1),m={__name:"161",setup(k){return u(a),f(),(g,x)=>(e(),t(r,p(o(l(d)(l(a),160))),{default:i(()=>[_,h]),_:1},16))}},T=c(m,[["__file","/@slidev/slides/161.md"]]);export{T as default};
--- slidev.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  theme: {
-    extend: {
-      spacing: {
-        'wide': '100px', // aggiungi la tua dimensione desiderata qui
-      }
-    }
-  }
-}
