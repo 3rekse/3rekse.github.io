1b71a246d706445812c2192a5defbf70c6229c3f
3rekse <bonfiglio.fabrizio@itiomar.net>
2024-05-13 21:58:07 +0200

    Updates

---
+++ assets/slidev/156-Cn5eBH1q.js
@@ -0,0 +1,13 @@
+import{o as e,c as t,k as i,q as p,s as r,A as l,e as s,a6 as a}from"../modules/vue-CzHRG3tJ.js";import{I as o}from"./image-right-Dy3JW3EN.js";import{_ as c,cF as n}from"../index-CLNHaTDe.js";import{p as f,u,f as d}from"./context-BF63dP51.js";import"./layoutHelper-C4hfVeg5.js";import"../modules/shiki-C5i64R19.js";const m=s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-"},[s("span",{class:"line"},[s("span",null,"let inputs = [false, true]")]),a(`
+`),s("span",{class:"line"},[s("span",null,"let Q = false")]),a(`
+`),s("span",{class:"line"},[s("span",null,"for (let A of inputs) {")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    Q = false")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    if (Q) {")]),a(`
+`),s("span",{class:"line"},[s("span",null,"        light.setAll(0x0000ff)")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    } else {")]),a(`
+`),s("span",{class:"line"},[s("span",null,"        light.setAll(0xff8000)")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    }")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    pause(500)")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    light.clear()")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    pause(500)")]),a(`
+`),s("span",{class:"line"},[s("span",null,"}")])])],-1),_={__name:"156",setup(h){return f(n),u(),(k,g)=>(e(),t(o,p(r(l(d)(l(n),155))),{default:i(()=>[m]),_:1},16))}},Q=c(_,[["__file","/@slidev/slides/156.md"]]);export{Q as default};
