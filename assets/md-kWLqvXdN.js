4bd69452254655a69cd4ac38ec79bc26ae3b6e29
3rekse <bonfiglio.fabrizio@itiomar.net>
2024-09-18 18:49:42 +0200

    Updates

---
+++ assets/md-kWLqvXdN.js
@@ -0,0 +1,15 @@
+import{o as t,c as p,k as o,e as s,aa as a,q as r,s as c,B as l}from"./modules/vue-DBgDjl02.js";import{_ as d}from"./slidev/cover.vue_vue_type_script_setup_true_lang-za4R_vZC.js";import{u,f as m}from"./slidev/context-DUtDxpjI.js";import"./index-BqoYv9XP.js";import"./modules/shiki-CR29Tdxk.js";const f={__name:"ilcomputer.md__slidev_110",setup(h){const{$slidev:k,$nav:g,$clicksContext:i,$clicks:_,$page:v,$renderContext:z,$frontmatter:n}=u();return i.setup(),(b,e)=>(t(),p(d,r(c(l(m)(l(n),109))),{default:o(()=>e[0]||(e[0]=[s("h3",null,"🧠 Omarillo-11",-1),s("h3",null,"La sottrazione nel sistema binario utilizza un concetto simile al prestito nel sistema decimale. Ad esempio, per sottrarre 011 da 101:",-1),s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"  101")]),a(`
+`),s("span",{class:"line"},[s("span",null,"- 011")]),a(`
+`),s("span",{class:"line"},[s("span",null,"-----")]),a(`
+`),s("span",{class:"line"},[s("span",null,"  010")]),a(`
+`),s("span",{class:"line"},[s("span")]),a(`
+`),s("span",{class:"line"},[s("span",null,`<CodeBlockWrapper v-bind="{}" :ranges='[]'>`)]),a(`
+`),s("span",{class:"line"},[s("span")])])],-1),s("p",null,"Per la moltiplicazione, il processo è simile alla moltiplicazione decimale ma utilizza solo 0 e 1. Moltiplicare binari è come fare una serie di addizioni shiftate. Ad esempio, per moltiplicare 101 per 11:",-1),s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span")]),a(`
+`),s("span",{class:"line"},[s("span",null,"</CodeBlockWrapper>")]),a(`
+`),s("span",{class:"line"},[s("span",null,"   101")]),a(`
+`),s("span",{class:"line"},[s("span",null,"×   11")]),a(`
+`),s("span",{class:"line"},[s("span",null,"-------")]),a(`
+`),s("span",{class:"line"},[s("span",null,"   101")]),a(`
+`),s("span",{class:"line"},[s("span",null,"+ 1010  (shiftato a sinistra di una posizione)")]),a(`
+`),s("span",{class:"line"},[s("span",null,"-------")]),a(`
+`),s("span",{class:"line"},[s("span",null," 1111")])])],-1),s("p",null,"Il risultato è 1111, che è 15 in decimale. 💻",-1)])),_:1},16))}},y=f;export{y as default};
