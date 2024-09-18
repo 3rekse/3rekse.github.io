5d2a46e7c6156ee964b9ac7461fdda88211d94c8
3rekse <bonfiglio.fabrizio@itiomar.net>
2024-09-18 11:54:20 +0200

    Updates

---
+++ assets/md-DXAAtf9Q.js
@@ -0,0 +1,35 @@
+import{o as c,c as t,k as o,q as u,s as r,B as e,p as d,a as _,e as s,aa as n}from"./modules/vue-9M24w38T.js";import{_ as m}from"./slidev/cover.vue_vue_type_script_setup_true_lang-BjWPLw3i.js";import{u as f,f as h}from"./slidev/context-BrpKgFPm.js";import{_ as g}from"./index-CErdVrjL.js";import"./modules/shiki-D165QSPi.js";const a=l=>(d("data-v-75751d9e"),l=l(),_(),l),k=a(()=>s("p",null,"🧠 Omarillo-4 Propongo una Struttura scalabile.",-1)),v=a(()=>s("p",null,"Quando il progetto cresce, è utile dividere il codice in moduli e componenti logici.",-1)),j=a(()=>s("div",{class:"scrollable"},[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-txt"},[s("span",{class:"line"},[s("span",null,"nome-progetto/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"├── node_modules/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"├── public/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   ├── index.html")]),n(`
+`),s("span",{class:"line"},[s("span",null,"├── src/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   ├── assets/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │   ├── images/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │   └── styles/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │       └── global.css")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   ├── components/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │   ├── Header/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │   │   ├── Header.js")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │   │   ├── Header.css")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │   │   └── Header.test.js")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │   └── Footer/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │       ├── Footer.js")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │       ├── Footer.css")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │       └── Footer.test.js")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   ├── pages/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │   ├── Home/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │   │   ├── Home.js")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │   │   ├── Home.css")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │   └── About/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │       ├── About.js")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │       └── About.css")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   ├── services/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │   └── api.js")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   ├── store/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   │   └── index.js")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   ├── App.js")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   ├── index.js")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│   └── utils/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"│       └── helpers.js")]),n(`
+`),s("span",{class:"line"},[s("span",null,"├── package.json")]),n(`
+`),s("span",{class:"line"},[s("span",null,"└── README.md")])])])],-1)),x={__name:"react.md__slidev_1061",setup(l){const{$slidev:b,$nav:$,$clicksContext:p,$clicks:H,$page:A,$renderContext:S,$frontmatter:i}=f();return p.setup(),(B,C)=>(c(),t(m,u(r(e(h)(e(i),1060))),{default:o(()=>[k,v,j]),_:1},16))}},E=g(x,[["__scopeId","data-v-75751d9e"]]);export{E as default};
