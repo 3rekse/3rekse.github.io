43d8b1862623f0e47a40ec1ea1914507cacf0a8c
3rekse <bonfiglio.fabrizio@itiomar.net>
2024-09-18 17:57:04 +0200

    Updates

---
+++ assets/md-VdVsSbxa.js
@@ -0,0 +1,16 @@
+import{o as p,c as t,k as c,e as s,aa as n,q as r,s as o,B as l}from"./modules/vue-DBgDjl02.js";import{_ as u}from"./slidev/cover.vue_vue_type_script_setup_true_lang-D80oyL7e.js";import{u as d,f as m}from"./slidev/context-XxWu7SsC.js";import"./index-Ba2uxMTG.js";import"./modules/shiki-CR29Tdxk.js";const f={__name:"react.md__slidev_1059",setup(_){const{$slidev:k,$nav:g,$clicksContext:e,$clicks:h,$page:x,$renderContext:$,$frontmatter:i}=d();return e.setup(),(v,a)=>(p(),t(u,r(o(l(m)(l(i),1058))),{default:c(()=>a[0]||(a[0]=[s("p",null,"🧠 - Omarillo-3",-1),s("h2",null,"Struttura iniziale di base",-1),s("p",null,[n("Di solito, la struttura di base generata da "),s("code",null,"create-react-app"),n(" è simile a questa:")],-1),s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-txt"},[s("span",{class:"line"},[s("span")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   nome-progetto/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   ├── node_modules/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   ├── public/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   ├── index.html")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   └── ...")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   ├── src/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   ├── App.css")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   ├── App.js")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   ├── App.test.js")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   ├── index.css")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   ├── index.js")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   ├── logo.svg")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   └── ...")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   ├── package.json")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   └── README.md")])])],-1)])),_:1},16))}},P=f;export{P as default};
