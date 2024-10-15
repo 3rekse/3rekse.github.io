78d834166161f9a35503baba72ad9b6b0340e2ab
3rekse <bonfiglio.fabrizio@itiomar.net>
2024-10-15 22:43:14 +0200

    Updates

---
+++ assets/slidev/default-DyGngE4l.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-DBgDjl02.js";import{_ as r}from"../index-3o7RyISY.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
