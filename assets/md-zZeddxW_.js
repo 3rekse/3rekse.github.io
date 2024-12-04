89d835545cd6795dacd01a0947ae227f76414e0a
3rekse <bonfiglio.fabrizio@itiomar.net>
2024-12-04 09:39:50 +0100

    Updates

---
+++ assets/md-zZeddxW_.js
@@ -0,0 +1,16 @@
+import{_ as u}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-CMShk9HR.js";import{o as p,c as r,k as e,e as s,l as c,m as d,aa as n,q as b,s as f,B as t}from"./modules/vue-DBgDjl02.js";import{_ as m}from"./slidev/cover.vue_vue_type_script_setup_true_lang-C20-6HIO.js";import{u as g,f as h}from"./slidev/context-CaCfZgp9.js";import"./modules/unplugin-icons-CvBUBcCb.js";import"./index-pdwLZ4Yc.js";import"./modules/shiki-CR29Tdxk.js";const _={__name:"algebra.md__slidev_453",setup(k){const{$slidev:x,$nav:$,$clicksContext:l,$clicks:w,$page:C,$renderContext:N,$frontmatter:o}=g();return l.setup(),(v,a)=>{const i=u;return p(),r(m,b(f(t(h)(t(o),452))),{default:e(()=>[a[1]||(a[1]=s("h2",null,"Chip set progetto 1",-1)),c(i,d({},{ranges:[]}),{default:e(()=>a[0]||(a[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"And(a= ,b= ,out= ) /* And gate */")]),n(`
+`),s("span",{class:"line"},[s("span",null,"And16(a= ,b= ,out= ) /* 16-bit And */")]),n(`
+`),s("span",{class:"line"},[s("span",null,"DMux(in= ,sel= ,a= ,b= ) /* Channels the input to one out of two outputs */")]),n(`
+`),s("span",{class:"line"},[s("span",null,"DMux4Way(in= ,sel= ,a= ,b= ,c= ,d= ) /* Channels the input to one out of four outputs */")]),n(`
+`),s("span",{class:"line"},[s("span",null,"DMux8Way(in= ,sel= ,a= ,b= ,c= ,d= ,e= ,f= ,g= ,h= ) /* Channels the input to one out of eight outputs */")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Mux(a= ,b= ,sel= ,out= ) /* Selects between two inputs */")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Mux16(a= ,b= ,sel= ,out= ) /* Selects between two 16-bit inputs */")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Mux4Way16(a= ,b= ,c= ,d= ,sel= ,out= ) /* Selects between four 16-bit inputs */")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Mux8Way16(a= ,b= ,c= ,d= ,e= ,f= ,g= ,h= ,sel= ,out= ) /* Selects between eight 16-bit inputs */")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Nand(a= ,b= ,out= ) /* Nand gate (built-in) */")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Not16(in= ,out= ) /* 16-bit Not */")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Not(in= ,out= ) /* Not gate */")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Or(a= ,b= ,out= ) /* Or gate */")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Or8Way(in= ,out= ) /* 8-way Or */")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Or16(a= ,b= ,out= ) /* 16-bit Or */")]),n(`
+`),s("span",{class:"line"},[s("span",null,"Xor(a= ,b= ,out= ) /* Xor gate */")])])],-1)])),_:1},16)]),_:1},16)}}},P=_;export{P as default};
