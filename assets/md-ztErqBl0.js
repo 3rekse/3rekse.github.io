67da54a4649df47cac4c5917baf2a6a74eb24009
3rekse <bonfiglio.fabrizio@itiomar.net>
2025-01-10 17:02:38 +0100

    Updates

---
+++ assets/md-ztErqBl0.js
@@ -0,0 +1,16 @@
+import{_ as d}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-BYVu6zK9.js";import{o as c,c as m,k as l,e as s,l as a,m as t,ac as i,q as f,s as g,B as o}from"./modules/vue-C12uiUEh.js";import{I as k}from"./slidev/two-cols-header-OgVjWvoq.js";import{u as z,f as v}from"./slidev/context-C63QEmo7.js";import"./modules/unplugin-icons-DJgmUv7V.js";import"./index-D4fxKu6o.js";import"./modules/shiki-BBIst5IO.js";const q={__name:"sequenziale.md__slidev_470",setup(h){const{$slidev:b,$nav:P,$clicksContext:r,$clicks:x,$page:$,$renderContext:B,$frontmatter:p}=z();return r.setup(),(O,n)=>{const e=d;return c(),m(k,f(g(o(v)(o(p),469))),{right:l(u=>[n[1]||(n[1]=s("p",null,"Vesione Simbolica Estesa",-1)),a(e,t({},{ranges:[]}),{default:l(()=>n[0]||(n[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"...")]),i(`
+`),s("span",{class:"line"},[s("span",null,"// Adds 1 to R1, repetitively")]),i(`
+`),s("span",{class:"line"},[s("span",null,"(LOOP) addi R1,1")]),i(`
+`),s("span",{class:"line"},[s("span",null,"...")]),i(`
+`),s("span",{class:"line"},[s("span",null,' goto LOOP //jmp LOOP "indirizzo fisico" ')]),i(`
+`),s("span",{class:"line"},[s("span")]),i(`
+`),s("span",{class:"line"},[s("span",null,"...   ")]),i(`
+`),s("span",{class:"line"},[s("span",null,"Programma ")]),i(`
+`),s("span",{class:"line"},[s("span",null,"+ Facile da realizzare")]),i(`
+`),s("span",{class:"line"},[s("span",null,"+ Leggibile ")]),i(`
+`),s("span",{class:"line"},[s("span",null,"+ Rilocabile")])])],-1)])),_:1},16)]),left:l(u=>[n[3]||(n[3]=s("p",null,"Vesione Simbolica Base",-1)),a(e,t({},{ranges:[]}),{default:l(()=>n[2]||(n[2]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"...")]),i(`
+`),s("span",{class:"line"},[s("span",null,"// Adds 1 to R1, repetitively")]),i(`
+`),s("span",{class:"line"},[s("span",null,"13 addi R1,1")]),i(`
+`),s("span",{class:"line"},[s("span",null,"...")]),i(`
+`),s("span",{class:"line"},[s("span",null,'27 goto 13 //jmp linea # 13 "indirizzo fisico" ')]),i(`
+`),s("span",{class:"line"},[s("span",null,"...")])])],-1)])),_:1},16)]),default:l(()=>[n[4]||(n[4]=s("h2",null,"Branching",-1)),n[5]||(n[5]=s("p",null,"Eseguire un’istruzione diversa da quella successiva in base a condizioni specifiche o a istruzioni di salto.",-1)),n[6]||(n[6]=s("ul",null,[s("li",null,[s("strong",null,"Esempio: Iniziare una nuova iterazione in un ciclo"),s("ul",null,[s("li",null,"Quando il programma decide di iniziare una nuova iterazione all’interno di un ciclo anziché eseguire l’istruzione successiva. Questo consente di ripetere o saltare parti specifiche del codice in base alle condizioni stabilite nel programma.")])])],-1))]),_:1},16)}}};export{q as default};
