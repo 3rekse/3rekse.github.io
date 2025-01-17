33d99d6a95dccb2c473d4b3523aec17bbb28b5b1
3rekse <bonfiglio.fabrizio@itiomar.net>
2025-01-17 16:45:41 +0100

    Updates

---
+++ assets/md-BmDFbTNF.js
@@ -0,0 +1,15 @@
+import{_ as r}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-BxnTZ3Jy.js";import{o,c as u,k as l,e as s,ac as n,l as c,m as d,q as m,s as f,B as e}from"./modules/vue-C12uiUEh.js";import{_ as g}from"./slidev/cover.vue_vue_type_script_setup_true_lang-DtiIpVvN.js";import{u as k,f as _}from"./slidev/context-CWGjG3BD.js";import"./modules/unplugin-icons-DJgmUv7V.js";import"./index-C6xm2-Qk.js";import"./modules/shiki-BBIst5IO.js";const D={__name:"react.md__slidev_1062",setup(x){const{$slidev:$,$nav:v,$clicksContext:t,$clicks:b,$page:h,$renderContext:B,$frontmatter:i}=k();return t.setup(),(C,a)=>{const p=r;return o(),u(g,m(f(e(_)(e(i),1061))),{default:l(()=>[a[1]||(a[1]=s("p",null,"🧠 - Omarillo-3",-1)),a[2]||(a[2]=s("h2",null,"Struttura iniziale di base",-1)),a[3]||(a[3]=s("p",null,[n("Di solito, la struttura di base generata da "),s("code",null,"create-react-app"),n(" è simile a questa:")],-1)),c(p,d({},{ranges:[]}),{default:l(()=>a[0]||(a[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-txt"},[s("span",{class:"line"},[s("span",null,"   nome-progetto/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   ├── node_modules/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   ├── public/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   ├── index.html")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   └── ...")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   ├── src/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   ├── App.css")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   ├── App.js")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   ├── App.test.js")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   ├── index.css")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   ├── index.js")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   ├── logo.svg")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   │   └── ...")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   ├── package.json")]),n(`
+`),s("span",{class:"line"},[s("span",null,"   └── README.md")])])],-1)])),_:1},16)]),_:1},16)}}};export{D as default};
