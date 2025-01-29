fe11619e209cf40cff1ed86185085a59d7b8cd2a
3rekse <bonfiglio.fabrizio@itiomar.net>
2025-01-29 19:01:05 +0100

    Updates

---
+++ assets/md-DG34yppo.js
@@ -0,0 +1,13 @@
+import{_ as r}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-Co81HYur.js";import{o,c,k as l,l as f,m as u,e as s,ac as a,q as m,s as d,B as n}from"./modules/vue-C12uiUEh.js";import{_}from"./slidev/image-right.vue_vue_type_script_setup_true_lang-47dtN5Qm.js";import{u as k,f as g}from"./slidev/context-CjmvcQqD.js";import"./modules/unplugin-icons-DJgmUv7V.js";import"./index-DNjLdreF.js";import"./modules/shiki-BBIst5IO.js";import"./slidev/layoutHelper-C4hfVeg5.js";const y={__name:"explorer.md__slidev_419",setup(h){const{$slidev:x,$nav:$,$clicksContext:t,$clicks:v,$page:B,$renderContext:C,$frontmatter:i}=k();return t.setup(),(P,e)=>{const p=r;return o(),c(_,m(d(n(g)(n(i),418))),{default:l(()=>[f(p,u({},{ranges:[]}),{default:l(()=>e[0]||(e[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"let inputs = [false, true]")]),a(`
+`),s("span",{class:"line"},[s("span",null,"let Q = false")]),a(`
+`),s("span",{class:"line"},[s("span",null,"for (let A of inputs) {")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    Q = false")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    if (Q) {")]),a(`
+`),s("span",{class:"line"},[s("span",null,"        light.setAll(0x0000ff)")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    } else {")]),a(`
+`),s("span",{class:"line"},[s("span",null,"        light.setAll(0xff8000)")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    }")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    pause(500)")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    light.clear()")]),a(`
+`),s("span",{class:"line"},[s("span",null,"    pause(500)")]),a(`
+`),s("span",{class:"line"},[s("span",null,"}")])])],-1)])),_:1},16)]),_:1},16)}}};export{y as default};
