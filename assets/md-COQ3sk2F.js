a02f4ed8cd1a808624dbd9ff712d0b8170f96944
3rekse <bonfiglio.fabrizio@itiomar.net>
2025-02-10 10:23:45 +0100

    Updates

---
+++ assets/md-COQ3sk2F.js
@@ -0,0 +1,11 @@
+import{_ as r}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-BA1tFU95.js";import{o as p,c,k as n,e as s,ac as e,l as d,m as u,q as m,s as f,B as t}from"./modules/vue-C12uiUEh.js";import{I as g}from"./slidev/default-fScDNtkv.js";import{u as h,f as k}from"./slidev/context-DC2NulPb.js";import"./modules/unplugin-icons-DJgmUv7V.js";import"./index-BHjyY0Hu.js";import"./modules/shiki-BBIst5IO.js";const R={__name:"algebra.md__slidev_400",setup(_){const{$slidev:b,$nav:v,$clicksContext:l,$clicks:x,$page:$,$renderContext:w,$frontmatter:i}=h();return l.setup(),(C,a)=>{const o=r;return p(),c(g,m(f(t(k)(t(i),399))),{default:n(()=>[a[1]||(a[1]=s("h2",null,[s("a",{href:"https://nand2tetris.github.io/web-ide/chip/",target:"_blank"},"Adder"),e(" : designed to add two n-bit numbers")],-1)),d(o,u({},{ranges:[]}),{default:n(()=>a[0]||(a[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-txt"},[s("span",{class:"line"},[s("span",null,"/**")]),e(`
+`),s("span",{class:"line"},[s("span",null," * 16-bit adder: Adds two 16-bit two's complement values.")]),e(`
+`),s("span",{class:"line"},[s("span",null," * The most significant carry bit is ignored.")]),e(`
+`),s("span",{class:"line"},[s("span",null," */")]),e(`
+`),s("span",{class:"line"},[s("span",null,"CHIP Add16 {")]),e(`
+`),s("span",{class:"line"},[s("span",null,"    IN a[16], b[16];")]),e(`
+`),s("span",{class:"line"},[s("span",null,"    OUT out[16];")]),e(`
+`),s("span",{class:"line"},[s("span")]),e(`
+`),s("span",{class:"line"},[s("span",null,"    PARTS:")]),e(`
+`),s("span",{class:"line"},[s("span",null,"    //// Replace this comment with your code.")]),e(`
+`),s("span",{class:"line"},[s("span",null,"}")])])],-1)])),_:1},16)]),_:1},16)}}};export{R as default};
