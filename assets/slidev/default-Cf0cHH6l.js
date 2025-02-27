751839861f27aa0f1e91f841b0bd9c33ed76458d
3rekse <bonfiglio.fabrizio@itiomar.net>
2025-02-27 19:31:40 +0100

    Updates

---
+++ assets/slidev/default-Cf0cHH6l.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-C12uiUEh.js";import{_ as r}from"../index-t07fq8qq.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
--- assets/slidev/default-Dzw6zp5z.js
+++
@@ -1,1 +0,0 @@
-import{o,b as t,f as s}from"../modules/vue-C12uiUEh.js";import{_ as r}from"../index-4Q2HV-WB.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
